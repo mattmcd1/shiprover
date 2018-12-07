a23ff72b058cc177573d4812cc4c08fba5f89f09
Matt <matt_mcdonald@live.com.au>
2018-12-07 15:40:27 +0800

    Added heartbeat. Added inline documentation. Removed global variables

--- rover1.py
+++ rover1.py
@@ -1,29 +1,46 @@
 import fnmatch
 import serial
-import queue
+from queue import Queue
 import threading
+import time
 
-device_msg_queue = queue.Queue(1000)
 
 def detect_serial_devices():
+    """ Function to detect FTDI USB-to-serial converters
+    Returns:
+      A list of serial device paths
+    """
     
     import glob
-    glist = glob.glob('/dev/serial/by-path/platform-3f980000.usb-usb-0:1.1.*')
+    glist = glob.glob('/dev/serial/by-path/platform-3f980000.usb-usb-0:1.*')
     ret = []
     
     for d in glist:
         ret.append(d)
     return ret
 
-def serial_device(device, deviceID):
+
+def serial_device(device, deviceID, message_queue, device_status):
+    """ Listens on a serial port, encapsulates data in a frame
+    with a RoverID (Raspberry Pi serial number) and DeviceID.
+    
+    Args:
+      device: An instance of a Serial object.
+      deviceID: An integer ID for that device
+    Returns:
+      Nothing.
+    """
+    
     head = 0x5a
     tail = 0xa5
     
     while True:
         line = device.read(100)
-        # print(line.decode('utf-8'))
+        
         if len(line) > 0:
-            # print('Received: ' + str(len(line)) + ' characters.')
+            device_status[deviceID] = 10        # will be decremented by heartbeat
+            
+            # Construct the frame
             frame = bytearray()
             frame.append(head)
             frame.append(head)
@@ -33,39 +50,82 @@ def serial_device(device, deviceID):
             frame+=(line)
             frame.append(tail)
             frame.append(tail)
-            device_msg_queue.put(frame)
+            
+            # Add the frame to the queue for uplink
+            message_queue.put(frame)
+    
+    return
 
 def get_serial():
-  # Extract serial from cpuinfo file
-  cpuserial = "0000000000000000"
-  try:
-    f = open('/proc/cpuinfo','r')
-    for line in f:
-      if line[0:6]=='Serial':
-        cpuserial = line[10:26]
-    f.close()
-  except:
-    cpuserial = "ERROR000000000"
+    """ Extract serial from cpuinfo file.
+    Used as the RoverID.
+    
+    Returns:
+      Raspberry Pi serial number.
+    """
+    
+    cpuserial = "0000000000000000"
+    try:
+        f = open('/proc/cpuinfo','r')
+        for line in f:
+          if line[0:6]=='Serial':
+            cpuserial = line[10:26]
+        f.close()
+    except:
+        cpuserial = "ERROR000000000"
 
-  return cpuserial
+    return cpuserial
+
+
+def beat(pulse_queue, period=1):
+    """ Heartbeat function. Used to generate an "alive" pulse.
+    """
+    
+    while True:
+        time.sleep(period)
+        pulse_queue.put(1)
+    
+    return
+
+def print_status(device_status):
+    """ Generates a status frame to be sent on the heartbeat pulse.
+    """
+    
+    head = 0x5a
+    tail = 0xa5
+    
+    frame = bytearray()
+    frame.append(head)
+    frame.append(head)
+    frame.extend(get_serial()[-4:].encode())
+    frame.append(0xFF)
+    frame+=(int(device_status[1]).to_bytes(1, byteorder='big'))
+    frame+=(int(device_status[2]).to_bytes(1, byteorder='big'))
+    frame+=(int(device_status[3]).to_bytes(1, byteorder='big'))
+    frame+=(int(device_status[4]).to_bytes(1, byteorder='big'))
+    frame.append(tail)
+    frame.append(tail)
+    
+    return frame
 
 def main():
     radio_port = ''
     gnss_correction_port = ''
     devices = []
+    device_status = [0, 0, 0, 0, 0]
+    device_msg_queue = Queue(1000)
+    pulse = Queue(1)                        # used for heartbeat
     
     serial_devices = detect_serial_devices()
     for port in serial_devices:
-        # print(port)
         if port == '/dev/serial/by-path/platform-3f980000.usb-usb-0:1.1.2:1.0-port0':
             radio_port = port
         else:
             devices.append(port)
-            if port == '/dev/serial/by-path/platform-3f980000.usb-usb-0:1.1.3:1.0-port0':
-                gnss_correction_port = port
     
     if radio_port != '':
         print('Radio connected on USB2.')
+        radio_serial = serial.Serial(radio_port, 115200, timeout=1)
     else:
         print('Radio not connected.')
     
@@ -77,31 +137,46 @@ def main():
     if len(devices) == 1:
         print(str(len(devices)) + ' device connected.')
     else:
-        print(str(len(devices)) + ' devices connected.')
+        print(str(len(devices)) + ' devices connected.')    
     
-    # radio_serial = serial.Serial(radio_port, 115200, timeout=1)
-    # gnss_correction_serial = serial.Serial(gnss_correction_port, 115200, timeout=1)
     
     if len(devices) > 0:
         serial1 = serial.Serial(devices[0], 115200, timeout=1)
-        thread1 = threading.Thread(target=serial_device, args=(serial1,1,),).start()
+        thread1 = threading.Thread(target=serial_device, args=(serial1,1,device_msg_queue,device_status,),).start()
+        if devices[0] == '/dev/serial/by-path/platform-3f980000.usb-usb-0:1.1.3:1.0-port0':
+            gnss_correction_port = serial1
     if len(devices) > 1:
-        serial1 = serial.Serial(devices[1], 115200, timeout=1)
-        thread1 = threading.Thread(target=serial_device, args=(serial2,2,),).start()
+        serial2 = serial.Serial(devices[1], 115200, timeout=1)
+        thread2 = threading.Thread(target=serial_device, args=(serial2,2,device_msg_queue,device_status,),).start()
+        if devices[1] == '/dev/serial/by-path/platform-3f980000.usb-usb-0:1.1.3:1.0-port0':
+            gnss_correction_port = serial2
     if len(devices) > 2:
-        serial1 = serial.Serial(devices[2], 115200, timeout=1)
-        thread1 = threading.Thread(target=serial_device, args=(serial3,3,),).start()
+        serial3 = serial.Serial(devices[2], 115200, timeout=1)
+        thread3 = threading.Thread(target=serial_device, args=(serial3,3,device_msg_queue,device_status,),).start()
+        if devices[2] == '/dev/serial/by-path/platform-3f980000.usb-usb-0:1.1.3:1.0-port0':
+            gnss_correction_port = serial3
     if len(devices) > 3:
-        serial1 = serial.Serial(devices[3], 115200, timeout=1)
-        thread1 = threading.Thread(target=serial_device, args=(serial4,4,),).start()
+        serial4 = serial.Serial(devices[3], 115200, timeout=1)
+        thread4 = threading.Thread(target=serial_device, args=(serial4,4,device_msg_queue,device_status,),).start()
+        if devices[3] == '/dev/serial/by-path/platform-3f980000.usb-usb-0:1.1.3:1.0-port0':
+            gnss_correction_port = serial4
     
+    heartbeat = threading.Thread(target=beat, args=(pulse, 5,),).start()
     
     while True:
         if not device_msg_queue.empty():
             data = device_msg_queue.get()
-            # radio_serial.write(data)
+            radio_serial.write(data)
             print(len(data))
             print(data)
+            
+        if not pulse.empty():
+            pulse.get()
+            for i in range(len(device_status)):
+                if device_status[i] > 0:
+                    device_status[i] -= 1
+            
+            print([hex(c) for c in print_status(device_status)])
     
     return 0
 
